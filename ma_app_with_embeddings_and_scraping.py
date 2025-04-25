e8c55b9b596272a89b0fd6d08997db398b0ff421
IgorLcmt <cmt.lasota.i@gmail.com>
2025-04-25 14:57:06 +0200

    Update ma_app_with_embeddings_and_scraping.py

--- ma_app_with_embeddings_and_scraping.py
+++ ma_app_with_embeddings_and_scraping.py
@@ -127,4 +127,4 @@ if api_key and query_input:
     except Exception as e:
         st.error(f"❌ Error: {e}")
 else:
-    st.info("<---- Enter company profile to begin.")
+    st.info(" Enter company profile to begin.")
