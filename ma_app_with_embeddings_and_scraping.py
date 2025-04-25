c17cd7e46a79eeb6caafc5a1926299e5f146efe6
IgorLcmt <cmt.lasota.i@gmail.com>
2025-04-25 12:47:44 +0200

    Update ma_app_with_embeddings_and_scraping.py

--- ma_app_with_embeddings_and_scraping.py
+++ ma_app_with_embeddings_and_scraping.py
@@ -127,4 +127,4 @@ if api_key and query_input:
     except Exception as e:
         st.error(f"❌ Error: {e}")
 else:
-    st.info("👉 Enter your OpenAI API key and a company profile to begin.")
+    st.info("👉 Enter company profile to begin.")
